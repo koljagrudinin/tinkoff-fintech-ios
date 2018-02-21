e3fff1cc2bf554f8f942e8531ce1f160cb8ac4d8
nikolay grudinin <koljagrudinin@gmail.com>
2018-02-21 19:43:12 +0300

    init

---
+++ FintechSchool/FintechSchool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FintechSchool
+//
+//  Created by Nikolay Grydinin on 21.02.18.
+//  Copyright © 2018 SNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red;
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
